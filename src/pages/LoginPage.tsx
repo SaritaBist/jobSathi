ee2c26d0105c9af135d77f8de3ebffb1afe67441
sarita <bsarita926@gmail.com>
2024-12-29 23:10:42 +0545

    chnages

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -8,7 +8,7 @@ const LoginPage=()=> {
         <div className=" w-[100vw] h-[100vh]  flex   ">
             <Login/>
             <div
-                className=" flex  flex-col  gap-4 items-center justify-center w-[50%]  h-full bg-mine-shaft-800 rounded-r-[200px] sm-mx:hidden ">
+                className=" flex  flex-col  gap-4 items-center justify-center w-[50%]  h-full bg-mine-shaft-800 rounded-l-[200px] sm-mx:hidden ">
                 <div className="flex  gap-1 items-center">
                     <IconBrandBebo className="text-bright-sun-400 h-16 w-16 md-mx:h-12 md-mx:w-12"/>
                     <div className="text-6xl text-bright-sun-400 bs-mx:text-5xl  md-mx:text-4xl">jobSathi</div>
